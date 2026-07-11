0618bbcd3b05faa0d575f1377cfbd8e8ee61ca7e
Anthony Thibault <ajt.hyperlogic@gmail.com>
2026-07-10 22:40:33 -0700

    added MatToTRS stub

--- src/loadgltf.c
+++ src/loadgltf.c
@@ -6,6 +6,7 @@
 #include "loadgltf.h"
 
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -15,6 +16,10 @@
 
 #define LOG_ERROR(fmt, ...) \
   fprintf(stderr, "ERROR: %s " fmt, __func__, ##__VA_ARGS__)
+
+static void MatToTRS(const float mat[16], HYA_Vec3 *t, HYA_Quat *r, HYA_Vec3 *s) {
+  // TODO:
+}
 
 static void PrintNode(cgltf_node *node, int indent_level) {
   for (int i = 0; i < indent_level; i++) printf("  ");
@@ -89,7 +94,9 @@ HYA_Result InitSkeletonFromGLTF(const char *filename, const char *root_joint,
     }
     shput(joint_map, joint->name, i);
     if (joint->has_matrix) {
-      // TODO: fix me
+      HYA_Vec3 scale;
+      MatToTRS(joint->matrix, skeleton->translations + i,
+                   skeleton->rotations + i, &scale);
       LOG_ERROR("joint->has_matrix not supported\n");
       return HYA_ERR_UNSUPPORTED;
     } else {
